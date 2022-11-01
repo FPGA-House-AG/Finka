b9f326ffcff51f99285d6447fb739610f5d1d297
Leon Woestenberg <leon@sidebranch.com>
2022-11-01 14:07:54 +0100

    Bring out B port of LookupMem in Axi4 component.
    Signed-off-by: Leon Woestenberg <leon@sidebranch.com>

--- hardware/scala/finka/LookupMem.scala
+++ hardware/scala/finka/LookupMem.scala
@@ -19,7 +19,7 @@ import scala.math.pow
 
 // true dual port ram with independent clocks, symmetric data widths
 case class LookupMem(memDataWidth : Int,
-                wordCount : Int) extends Component {
+                     wordCount : Int) extends Component {
 
   //val bram_bus_config = BRAMConfig(memDataWidth, log2Up(wordCount))
 
@@ -158,7 +158,7 @@ case class LookupMem(memDataWidth : Int,
     // index of CPU word inside the addressed memory word
     val wr_cpu_word_of_memory_word = (busCtrl.writeAddress >> bytes_to_cpu_word_shift).resize(cpu_word_to_memory_word_shift) & U(cpu_word_to_memory_word_mask, cpu_word_to_memory_word_shift bits)
 
-    val expected = Reg(UInt(widthOf(busCtrl.writeAddress) bits))
+    val expected_bus_write_addr = Reg(UInt(widthOf(busCtrl.writeAddress) bits))
 
     // accumulated data to be written to memory in one cycle
     val write_data_width = cpu_words_per_memory_word * busCtrl.busDataWidth
@@ -166,13 +166,13 @@ case class LookupMem(memDataWidth : Int,
     
     // first CPU word of a memory word is written on the bus
     when (is_written_first) {
-      expected := busCtrl.writeAddress + busCtrl.busDataWidth / 8
+      expected_bus_write_addr := busCtrl.writeAddress + busCtrl.busDataWidth / 8
       // write first CPU word in most significant CPU word (shifted down if more CPU words follow), clear other bits
       write_data := bus_wr_data.resize(write_data_width) |<< ((cpu_words_per_memory_word - 1) * busCtrl.busDataWidth)
     // expected next CPU word of a memory word is written on the bus
     }
-    .elsewhen (is_written & (expected === busCtrl.writeAddress)) {
-      expected := busCtrl.writeAddress + busCtrl.busDataWidth / 8
+    .elsewhen (is_written & (expected_bus_write_addr === busCtrl.writeAddress)) {
+      expected_bus_write_addr := busCtrl.writeAddress + busCtrl.busDataWidth / 8
       // write new CPU word in most significant CPU word, shift down the existing accumulated data
       write_data := (bus_wr_data.resize(write_data_width) |<< ((cpu_words_per_memory_word - 1) * busCtrl.busDataWidth)) | (write_data |>> busCtrl.busDataWidth)
     }
@@ -203,17 +203,19 @@ case class LookupMem(memDataWidth : Int,
 
     val mem_read_addr = UInt(memAddressWidth bits)
     mem_read_addr := (busCtrl.readAddress >> bytes_to_memory_word_shift).resize(memAddressWidth)
+    val expected_bus_read_addr = Reg(UInt(widthOf(busCtrl.readAddress) bits))
+
 
     // addresses the CPU word inside the memory word- @TODO what if zero?
     val read_cpu_word_of_memory_word = UInt(cpu_word_to_memory_word_shift bits)
     // calculate which CPU word is addressed, then reduce to only the CPU word index inside the memory word
     read_cpu_word_of_memory_word := (busCtrl.readAddress >> bytes_to_cpu_word_shift).resize(cpu_word_to_memory_word_shift) & U(cpu_word_to_memory_word_mask, cpu_word_to_memory_word_shift bits)
 
-    //val bus_read_data = Bits(busCtrl.busDataWidth bits)
-    //// @TODO this might be expensive due to the MUX for variable number 'read_cpu_word_of_memory_word'
-    //// @TODO maybe also only allow sequential read access to all CPU words in memory, like with write?
-    //bus_read_data := (io.portA.rdData >> (read_cpu_word_of_memory_word * busCtrl.busDataWidth)).resize(busCtrl.busDataWidth)
-    //busCtrl.readPrimitive(bus_read_data, SizeMapping(0, memory_size), 0, documentation = null)
+    val bus_read_data = Bits(busCtrl.busDataWidth bits)
+    // @TODO this might be expensive due to the MUX for variable number 'read_cpu_word_of_memory_word'
+    // @TODO maybe also only allow sequential read access to all CPU words in memory, like with write?
+    bus_read_data := (io.portA.rdData >> (read_cpu_word_of_memory_word * busCtrl.busDataWidth)).resize(busCtrl.busDataWidth)
+    busCtrl.readPrimitive(bus_read_data, SizeMapping(0, memory_size), 0, documentation = null)
 
     // drive read address on memory
     io.portA.addr := mem_read_addr
@@ -224,13 +226,6 @@ case class LookupMem(memDataWidth : Int,
     io.portA.wrData := write_data.resize(memDataWidth)
     io.portA.wr := RegNext(is_written_last)
 
-    io.portB.en := True
-    io.portB.wr := False
-    io.portB.clk := False
-    io.portB.rst := False
-    io.portB.addr := 0
-    io.portB.wrData := 0
-
     // SpinalHDL/lib/src/main/scala/spinal/lib/com/usb/udc/UsbDeviceCtrl.scala
 
     val readState = RegInit(U"00")
@@ -240,9 +235,8 @@ case class LookupMem(memDataWidth : Int,
     val all = Bool()
 
     // if haltSentive is false, then the call back is made durring the whole access, while if that's true it is only done the last cycle of the access.
-
+    // haltSensitive = false => all cycles
     all := False
-
     busCtrl.onReadPrimitive(SizeMapping(0, memory_size), haltSensitive = false, documentation = null) {
       all := True
       switch(readState){
@@ -256,11 +250,11 @@ case class LookupMem(memDataWidth : Int,
         }
       }
     }
+    // haltSensitive = true => only on last cycle
     cycle := False
     busCtrl.onReadPrimitive(SizeMapping(0, memory_size), haltSensitive = true, documentation = null) {
       readState := 0
       cycle := True
-
       //busCtrl.readHalt()
     }
 
@@ -321,12 +315,33 @@ case class LookupMemAxi4(wordWidth : Int, wordCount : Int, busCfg : Axi4Config) 
   // copy AXI4 properties from bus, but override address width for slave
   val slaveCfg = busCfg.copy(addressWidth = memory_space_address_bits)
 
+  val memAddressWidth = log2Up(wordCount)
+
   val io = new Bundle {
+    // bus controller slave used to update the lookup table over a bus
     val ctrlbus = slave(Axi4(slaveCfg))
+
+    // lookup
+    val clk = in Bool()
+    val rst = in Bool()
+    val en = in Bool()
+    val wr = in Bool()
+    val addr = in UInt (memAddressWidth bits)
+    val wrData = in Bits (wordWidth bits)
+    val rdData = out Bits (wordWidth bits)
   }
+
   val mem = LookupMem(wordWidth, wordCount)
   val ctrl = new Axi4SlaveFactory(io.ctrlbus)
   val bridge = mem.driveFrom(ctrl)
+
+  mem.io.portB.clk := io.clk
+  mem.io.portB.rst := io.rst
+  mem.io.portB.en := io.en
+  mem.io.portB.wr := io.wr
+  mem.io.portB.addr := io.addr
+  mem.io.portB.wrData := io.wrData
+  io.rdData := mem.io.portB.rdData
 }
 
 //Generate the CorundumFrameFilter's Verilog
